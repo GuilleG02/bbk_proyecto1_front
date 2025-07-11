4ac5b72aeb6de32733e2495ae487e3ccc3290547
SantiagoOH21 <soh0621@gmail.com>
2025-07-11 20:11:59 +0200

    Routes added in App.jsx & Header rename

--- proyecto_front/src/App.jsx
+++ proyecto_front/src/App.jsx
@@ -1,19 +1,38 @@
 import { useState } from "react";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./views/Home";
-import Header from "./components/Header";
+import Products from "./views/Products";
+import TheHeader from "./components/TheHeader";
 import Footer from "./components/Footer";
+import Register from "./views/Register";
+import Login from "./views/Login";
+import Profile from "./views/Profile";
+import Cart from "./views/Cart";
+import Checkout from "./views/Checkout";
+
+const mockCart = [
+  { id: 1, name: "Producto A", price: 50, quantity: 2 },
+  { id: 2, name: "Producto B", price: 30, quantity: 1 },
+];
 
 function App() {
   return (
-    <BrowserRouter>
-      <Header />
+    <Router>
+      <TheHeader />
       <main className="main-content">
-        <Home />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/checkout" element={<Checkout cartItems={mockCart} />} />
+        </Routes>
       </main>
       <Footer />
-    </BrowserRouter>
+    </Router>
   );
 }
 
---
+++ proyecto_front/src/views/Products.jsx
@@ -0,0 +1,9 @@
+//Borra esto//
+import React from "react";
+
+const Products = () => {
+  return <div>Products</div>;
+};
+
+export default Products;
+//Borra esto//
